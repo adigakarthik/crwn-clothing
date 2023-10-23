4d2d8c6f7241454e46a14d29041ed764cfa49679
Karthik Vadhiraj <karthik.vadhiraj@ccmllc.com>
2023-10-23 15:58:24 -0500

    modularized to components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.style.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((m) => (
+        <CategoryItem key={m.id} category={m} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
