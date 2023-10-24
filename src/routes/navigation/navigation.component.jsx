985fe9fd89b9f110dbf561fe1b18dea23a71f87e
Karthik Vadhiraj <karthik.vadhiraj@ccmllc.com>
2023-10-23 21:46:12 -0500

    formatting

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,15 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './navigation.style.scss'
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./navigation.style.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <Link className="logo-container" to='/'>
-            <Logo className="logo"/>
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
